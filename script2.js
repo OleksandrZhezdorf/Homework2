78f620b1f075d8d92607326bd7f3f62d90547f6d
Oleksandr Zhezdorf <aleks@gmail.com>
2020-11-20 00:20:12 +0200

    Second change

--- script2.js
+++ script2.js
@@ -1,8 +1,20 @@
 /*Среднее арифметическое*/
 
-let digit1 = prompt('First number');
-let digit2 = prompt('Second number');
-let digit3 = prompt('Third number');
-let digit4 = prompt('Its almost done');
-let digit5 = prompt('Last number');
-console.log((digit1/5 + digit2/5 + digit3/5 + digit4/5 + digit5/5));
+let digit1 = Number(prompt('First number'));
+let digit2 = Number(prompt('Second number'));
+let digit3 = Number(prompt('Third number'));
+let digit4 = Number(prompt('Its almost done'));
+let digit5 = Number(prompt('Last number'));
+let result = (digit1 + digit2 + digit3 + digit4 + digit5) / 5
+
+
+console.log(result);
+
+/*Усложненный вариант */
+
+let digit = prompt('Write your numbers', 123);
+
+let sum = digit.split(",").reduce(function (a, b) { return a + +b }, 0) / digit.split(",").length;
+
+console.log(sum);
+
